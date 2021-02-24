2250b5bd3e83536378dfa2bf3f6c097b99cc14ed
JareBear <jaredgrozz@gmail.com>
2021-02-24 11:36:13 -0600

    Update Book_Generator.py

--- Scripts/Book_Generator.py
+++ Scripts/Book_Generator.py
@@ -9,19 +9,23 @@ import os
 import numpy as np
 import cv2
 from natsort import natsort_keygen
+from fpdf import FPDF
 
 # Declare constant variables
 NATSORT_KEY = natsort_keygen()
 TEXT_FILE = 'Scripts/output.txt'
-IMAGES_LOCATION = 'Scripts/Output/'
+IMAGES_LOCATION = 'Scrpts/Images/'
+if not os.path.exists(IMAGES_LOCATION): os.makedirs(IMAGES_LOCATION)
+OUTPUT_LOCATION = 'Scripts/Output/'
+if not os.path.exists(OUTPUT_LOCATION): os.makedirs(OUTPUT_LOCATION)
 TEXT_FILE_CONTENTS = None
 
 # Output options
 DPI = 300
-TOP_MARGIN = int(0.75*DPI) # Inches
-BOTTOM_MARGIN = int(0.75*DPI) # Inches
+TOP_MARGIN = int(0.75*DPI)  # Inches
+BOTTOM_MARGIN = int(0.75*DPI)  # Inches
 LEFT_MARGIN = int(0.75*DPI)  # Inches
-RIGHT_MARGIN = int(0.75*DPI) # Inches
+RIGHT_MARGIN = int(0.75*DPI)  # Inches
 MARGIN_SIZE = (TOP_MARGIN, BOTTOM_MARGIN, LEFT_MARGIN, RIGHT_MARGIN)
 PAPER_SIZE = (11, 8.5)  # INCHES
 PIXEL_SIZE = (int(PAPER_SIZE[0]*DPI), int(PAPER_SIZE[1]*DPI))
@@ -38,6 +42,11 @@ CHARACTERS_PER_LINE = 40
 
 FONT_SIZE = 49
 FONT = ImageFont.truetype('arial.ttf', FONT_SIZE)
+
+'''
+<COL_EMPTY> Leave coloumn empty and start in next one.
+<TITLE> Your title </TITLE> WIll give 2 lines extra space to add title.
+'''
 
 def clear_folders(folders):
     for folder in folders:
@@ -69,6 +78,7 @@ def change_orientation(document, new_section):
     new_section.page_height = new_height
     return new_section
 
+
 def convert_text_to_image():
     # Declare local variables
     orginized_text_file_contents = []
@@ -78,11 +88,14 @@ def convert_text_to_image():
     line_count = 0
 
     # Read text file
-    with open(TEXT_FILE, 'r', encoding='utf-8') as file: TEXT_FILE_CONTENTS = file.read()
+    with open(TEXT_FILE, 'r', encoding='utf-8') as file:
+        TEXT_FILE_CONTENTS = file.read()
 
     # Orginize text to fit on pages (start to end)
     lines = TEXT_FILE_CONTENTS.split('\n')
     text = []
+    found_title = False
+    titles = ''
     for line in lines:
         text += line.split(' ')
     for _, word in enumerate(text):
@@ -91,29 +104,62 @@ def convert_text_to_image():
             orginized_text_file_contents.append(['\n'])
             orginized_line = ''
             char_count = 0
-            # continue
+        elif word == '<TITLE>':
+            if len(orginized_line) > 0:
+                orginized_text_file_contents.append([orginized_line])
+                orginized_line = ''
+                char_count = 0
+            found_title = True
+            continue
+        elif word == '</TITLE>' and found_title:
+            found_title = False
+            orginized_text_file_contents.append(['\n'])
+            orginized_text_file_contents.append([titles])
+            orginized_text_file_contents.append(['\n'])
+            continue
+        elif word == '<COL_EMPTY>':
+            if len(orginized_line) > 0:
+                orginized_text_file_contents.append([orginized_line])
+                orginized_line = ''
+                char_count = 0
+            orginized_text_file_contents.append([word])
+            continue
+
+        if found_title:
+            titles += (word + ' ')
+            continue
+
         char_count += len(word)
         orginized_line += (word + ' ')
         if char_count >= CHARACTERS_PER_LINE:
             orginized_text_file_contents.append([orginized_line])
             orginized_line = ''
             char_count = 0
+
     # Put lines in order of pages
     page_text = []
-    for _, line in enumerate(orginized_text_file_contents):
+    for index, line in enumerate(orginized_text_file_contents):
+        if line == ['<COL_EMPTY>']:
+            for _ in range(LINES_PER_PAGE-len(page_text)):
+                line_count += 1
+                page_text.append(['\n'])
+            page_layout_text.append(page_text)
+            line_count = 0
+            page_text = []
+            continue
         line_count += 1
         page_text.append(line)
-        if line_count >= LINES_PER_PAGE:
+        if line_count >= LINES_PER_PAGE or index == len(orginized_text_file_contents)-1:
+            print(index, len(orginized_text_file_contents))
             page_layout_text.append(page_text)
             page_text = []
             line_count = 0
     page_text.clear()
 
     # How many coloumns are there
-    for i in orginized_text_file_contents[(len(page_layout_text)*LINES_PER_PAGE):]: page_text.append(i)
-    page_layout_text.append(page_text)
     NUM_OF_COLS = (len(page_layout_text))
-    if NUM_OF_COLS % 2 != 0: NUM_OF_COLS += 1
+    if NUM_OF_COLS % 2 != 0:
+        NUM_OF_COLS += 1
 
     # Figure out order of pages
     ORDER_OF_PAGES = [['BACK', 'FRONT']]
@@ -121,13 +167,15 @@ def convert_text_to_image():
     for i in range(NUM_OF_COLS):
         n = i + 1
         if not i >= (NUM_OF_COLS//2):
-            ORDER_OF_PAGES.append([n if flip_spots else NUM_OF_COLS-i, NUM_OF_COLS-i if flip_spots else n])
+            ORDER_OF_PAGES.append(
+                [n if flip_spots else NUM_OF_COLS-i, NUM_OF_COLS-i if flip_spots else n])
             flip_spots = not flip_spots
 
     # Create blank image
     file_names = []
     for i in ORDER_OF_PAGES:
-        for j in i: file_names.append(j)
+        for j in i:
+            file_names.append(j)
 
     for page_name in file_names:
         img = Image.new("RGB", COL_SIZE, (255, 255, 255))
@@ -139,24 +187,28 @@ def convert_text_to_image():
     all_file_names.pop(-1)
     all_file_names.pop(-1)
     for page_number in all_file_names:
-        page_number = int(page_number.replace('.png',''))
+        page_number = int(page_number.replace('.png', ''))
         try:
-            if page_number == 0: continue
+            if page_number == 0:
+                continue
             img = Image.open(f"{IMAGES_LOCATION}{page_number}.png")
             text = [i[0] for i in page_layout_text[page_number-1]]
             text = '\n'.join(text)
             text_img = ImageDraw.Draw(img)
             text_img.text((0, 0), text, fill=(0, 0, 0), font=FONT)
-            img = add_margin(img, TOP_MARGIN, RIGHT_MARGIN, BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
+            img = add_margin(img, TOP_MARGIN, RIGHT_MARGIN,
+                             BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
             img.save(f"{IMAGES_LOCATION}{page_number}.png", quality=95)
         except IndexError:
             print(f'Page #{page_number} is empty.')
             img = Image.open(f"{IMAGES_LOCATION}{page_number}.png")
-            img = add_margin(img, TOP_MARGIN, RIGHT_MARGIN, BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
+            img = add_margin(img, TOP_MARGIN, RIGHT_MARGIN,
+                             BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
             img.save(f"{IMAGES_LOCATION}{page_number}.png", quality=95)
     for page_name in ['BACK', 'FRONT']:
         img = Image.open(f"{IMAGES_LOCATION}{page_name}.png")
-        img = add_margin(img, TOP_MARGIN, RIGHT_MARGIN, BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
+        img = add_margin(img, TOP_MARGIN, RIGHT_MARGIN,
+                         BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
         img.save(f"{IMAGES_LOCATION}{page_name}.png", quality=95)
 
     # Add page numbers in header
@@ -167,18 +219,20 @@ def convert_text_to_image():
                 img = Image.open(f"{IMAGES_LOCATION}{page}.png")
                 W, H = img.size
                 # w, h = page_num_img.textsize(str(page))
-                w,h = FONT.getsize(str(page))
+                w, h = FONT.getsize(str(page))
                 page_num_img = ImageDraw.Draw(img)
                 # Add page_numbers
                 if PAGE_NUMBERS_TOP:
                     if flip_spots:
-                        page_num_img.text((100, 100), str(page), fill=(0, 0, 0), font=FONT)
+                        page_num_img.text((100, 100), str(
+                            page), fill=(0, 0, 0), font=FONT)
                     else:
                         page_num_img.text((W-w-150, 100), str(page),
-                                        fill=(0, 0, 0), font=FONT)
+                                          fill=(0, 0, 0), font=FONT)
                     flip_spots = not flip_spots
                 elif PAGE_NUMBERS_BOTTOM:
-                    page_num_img.text(((W-w)/2,(H-h-150)), str(page), fill=(0, 0, 0), font=FONT)
+                    page_num_img.text(((W-w)/2, (H-h-150)),
+                                      str(page), fill=(0, 0, 0), font=FONT)
                 img.save(f"{IMAGES_LOCATION}{page}.png", quality=95)
 
     # Combine
@@ -190,19 +244,23 @@ def convert_text_to_image():
         except:
             img = Image.open(f"{IMAGES_LOCATION}{page_order[1]}.png")
             img = add_margin(img, TOP_MARGIN, RIGHT_MARGIN,
-                            BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
+                             BOTTOM_MARGIN, LEFT_MARGIN, (255, 255, 255))
             img.save(f"{IMAGES_LOCATION}{page_order[1]}.png", quality=95)
             vis = np.concatenate((img1, img), axis=1)
         # vis = cv2.hconcat([img1, img2])
-        cv2.imwrite(f'{IMAGES_LOCATION}Page {page_index+1}; {page_order[0]} - {page_order[1]}.png', vis)
+        cv2.imwrite(
+            f'{IMAGES_LOCATION}Page {page_index+1}; {page_order[0]} - {page_order[1]}.png', vis)
         if page_index == 0:
-            img = Image.open(f'{IMAGES_LOCATION}Page {page_index+1}; {page_order[0]} - {page_order[1]}.png')
+            img = Image.open(
+                f'{IMAGES_LOCATION}Page {page_index+1}; {page_order[0]} - {page_order[1]}.png')
             draw = ImageDraw.Draw(img)
             W, H = img.size
             draw.line((W/2, 0, W/2, H), fill=(0, 0, 0), width=1)
-            img.save(f'{IMAGES_LOCATION}Page {page_index+1}; {page_order[0]} - {page_order[1]}.png', quality=95)
+            img.save(
+                f'{IMAGES_LOCATION}Page {page_index+1}; {page_order[0]} - {page_order[1]}.png', quality=95)
         os.remove(f"{IMAGES_LOCATION}{page_order[0]}.png")
         os.remove(f"{IMAGES_LOCATION}{page_order[1]}.png")
+
 
 def convert_image_to_document():
     # Compile and Save
@@ -219,10 +277,26 @@ def convert_image_to_document():
         section.left_margin = Inches(0)
         section.right_margin = Inches(0)
     for _, path in enumerate(all_file_names):
-        document.add_picture(IMAGES_LOCATION + path, width=Inches(PAPER_SIZE[0]))
-    document.save('Scripts/Output/Compiled.docx')
+        document.add_picture(IMAGES_LOCATION + path,
+                             width=Inches(PAPER_SIZE[0]))
+    document.save(f'{OUTPUT_LOCATION}/Compiled.docx')
+
+def convert_image_to_pdf():
+    all_file_names = os.listdir(IMAGES_LOCATION)
+    all_file_names.sort(key=NATSORT_KEY)
+
+    pdf = FPDF('L', 'in', 'A4')
+
+    # Convert to mm
+    LEFT_MARGIN, RIGHT_MARGIN = 0, 0
+
+    for image in all_file_names:
+        pdf.add_page()
+        pdf.image(IMAGES_LOCATION + image, LEFT_MARGIN, RIGHT_MARGIN, PAPER_SIZE[0], PAPER_SIZE[-1])
+    pdf.output(f"{OUTPUT_LOCATION}/Compiled.pdf", "F")
 
 if __name__ == '__main__':
     clear_folders([IMAGES_LOCATION])
     convert_text_to_image()
     convert_image_to_document()
+    convert_image_to_pdf()
